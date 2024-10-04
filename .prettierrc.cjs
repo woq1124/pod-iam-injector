ca82e72ac1af89740d8d6d03a84a40b22183bd95
Vin (이재빈) <jb.lee@ecubelabs.com>
2024-10-04 12:32:40 +0900

    First Commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  tabWidth: 4,
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'lf',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  overrides: [
+    {
+      files: '{**/.vscode/*.json,**/tsconfig.json,**/tsconfig.*.json}',
+      options: {
+        parser: 'json5',
+        quoteProps: 'preserve',
+        singleQuote: false,
+        trailingComma: 'all',
+      },
+    },
+  ],
+};
