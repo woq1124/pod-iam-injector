6f32b0996bff55c48ce9874594d8a842545218d0
LJB <woqls4@gmail.com>
2024-10-07 01:21:08 +0900

    refac: port config

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -3,3 +3,5 @@ export const AUDIENCE = process.env.AUDIENCE ?? 'sts.amazonaws.com';
 export const CERTIFICATE_PATH = process.env.CERTIFICATE_PATH ?? '/etc/webhook/certs';
 export const JSON_WEB_KEY_PREFIX = process.env.JSON_WEB_KEY_PREFIX ?? 'json-web-key';
 export const JSON_WEB_KEY_COUNT = process.env.JSON_WEB_KEY_COUNT ? parseInt(process.env.JSON_WEB_KEY_COUNT) : 4;
+export const OIDC_PROVIDER_SERVER_PORT = 8080;
+export const MUTATE_SEVER_PORT = 8443;
